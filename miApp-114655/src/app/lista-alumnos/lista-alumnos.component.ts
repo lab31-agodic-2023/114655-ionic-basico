6655e1786fcaa0272f9ac685818b17b944a79b37
Luz <luz.d.saucedo@kp.org>
2023-10-03 19:25:45 -0600

    Action Sheet para Ionic 6

--- miApp-114655/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp-114655/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,19 +1,78 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
   styleUrls: ['./lista-alumnos.component.scss'],
 })
 export class ListaAlumnosComponent  implements OnInit {
+  //constructor
+  constructor(private actionSheetCtrl: ActionSheetController) { }
 
-  constructor() { }
 
-  ngOnInit() {}
-
+  //variables
+  result: string = '';
   alumnos : any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
 
-  numero : number = 0;
+  //ngOnInit
+  ngOnInit() {}
+//Action Sheet Ionic 6
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Example header',
+      subHeader: 'Example subheader',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Share',
+          data: {
+            action: 'share',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
 
-  verdadero: boolean = true;
+    await actionSheet.present();
+
+    const result = await actionSheet.onDidDismiss();
+    this.result = JSON.stringify(result, null, 2);
+  }
+
+  //Action Sheet Ionic 7
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Share',
+      data: {
+        action: 'share',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
 }
